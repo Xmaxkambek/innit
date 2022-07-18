5ef0a7171ae8924733a49434cc94638be9da8672
nnn <maxkambekxolbekov515@gmail.com>
2022-07-17 21:17:37 -0700

    print

---
+++ bin/print.dart
@@ -0,0 +1,21 @@
+void main(){
+    print (' _____________________________');
+    print ('|                             |');
+    print ('|        STEVE JOBS           |');
+    print ('|                             |');
+    print ('| born:february 24.1955       |');
+    print ('| died:october 5.2011         |');
+    print ('|                             |');
+    print ('|                             |');
+    print ('| co-founder of Apple         |');
+    print ('| his inventions transformed  |');
+    print ('| tech and communication      |');
+    print ('| .was SEO of Apple when the  |');
+    print ('| iphone was first introduced |');
+    print ('| .Also known for investing   |');
+    print ('| in Pixar when they were a   |');
+    print ('| struggling businus          |');
+    print ('|                             |');
+    print ('|                             |');
+    print ('|_____________________________|');
+    }
